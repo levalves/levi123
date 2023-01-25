085adb8763289ae90e001c80eb9c891eb7a3ce19
Levinux <levi.linux@hotmail.com>
2023-01-25 10:16:58 -0300

    Updated

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "this" {
+  bucket = "my-bucket-levinux"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Hash        = "abc123"
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
